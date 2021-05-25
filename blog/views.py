952559d43e190cac6fdfc86cde4a68e3a07c9da8
User <kagadey2@gmail.com>
2021-05-25 06:43:08 +0300

    Tagging sys

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,9 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from django.core.mail import send_mail
+from django.db.models import Count
+from taggit.models import Tag
+
 
 from .forms import EmailPostForm, CommentForm
 from .models import Post, Comment
@@ -18,8 +21,12 @@ from .models import Post, Comment
 #     template_name = "blog/post/list.html"
 
 
-def post_list(requst):
+def post_list(requst, tag_slug=None):
     object_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
     paginator = Paginator(object_list, 3)
     page = requst.GET.get('page')
     try:
@@ -30,7 +37,7 @@ def post_list(requst):
         posts = paginator.page(paginator.num_pages)
 
     # posts = Post.published.all()
-    return render(requst, 'blog/post/list.html', {'page':page, 'posts': posts})
+    return render(requst, 'blog/post/list.html', {'page':page, 'posts': posts, 'tag':tag})
 
 
 def post_details(request, year, month, day, post):
@@ -45,8 +52,10 @@ def post_details(request, year, month, day, post):
             new_comment.save()
     else:
         comment_form = CommentForm()
-
-    return render(request, 'blog/post/details.html', {'post': post, 'comments': comments, 'new_comment': new_comment, 'comment_form': comment_form})
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
+    return render(request, 'blog/post/details.html', {'post': post, 'comments': comments, 'new_comment': new_comment, 'comment_form': comment_form, 'similar_posts':similar_posts})
 
 def post_share(request, post_id):
     post = get_object_or_404(Post, id=post_id, status='published')
--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    #Third party
+    'taggit',
 
     #Local
     'blog.apps.BlogConfig',
