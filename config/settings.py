1da5ce95e4b7c74b04c75282d112abb57c7d1273
User <kagadey2@gmail.com>
2021-05-31 19:24:22 +0300

    Text searching

--- config/settings.py
+++ config/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'django.contrib.sites',
     'django.contrib.sitemaps',
 
-
     #Third party
     'taggit',
 
@@ -86,8 +85,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'new_blog_db',
+        'USER': 'postgres',
+        'PASSWORD': 'ghbdtn8691PO',
     }
 }
 
